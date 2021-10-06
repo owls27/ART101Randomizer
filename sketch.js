5d54349341221e36fbdc3066bb909c5ae19f971f
owls27 <tzuhyo0927@gmail.com>
2021-10-05 19:59:55 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+}
+
+function draw() {
+  var locX = random(0, 800);
+  var locY = random(0, 800);
+  var s = random(5, 10);
+  square(locX, locY, s);
+  
+  if (mouseIsPressed) {
+    var r = random(1, 5);
+    noStroke();
+    fill(0, 60);
+    circle(mouseX, mouseY, r);
+    strokeWeight(r);
+    stroke(0.100);
+    line(pmouseX, pmouseY, mouseX, mouseY)
+  }
 }
